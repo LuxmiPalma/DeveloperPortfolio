4c029b5a204e1efca949c8d5eb8e54759da8fc87
LuxmiPalma <luxmi.palma@gmail.com>
2025-05-14 12:09:45 +0200

    push Create the AppDbContext

---
+++ ProjectsApi/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ProjectsApi.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ProjectsApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Project> Projects { get; set; }
+    }
+}
