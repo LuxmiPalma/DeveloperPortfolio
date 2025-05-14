d50ea2fc747eb13dc6069ef09bda5e2929922bc8
LuxmiPalma <luxmi.palma@gmail.com>
2025-05-14 12:16:59 +0200

    Create DataInitializer Class for Seeding

---
+++ ProjectsApi/Data/DataInitializer.cs
@@ -0,0 +1,36 @@
+﻿using ProjectsApi.Models;
+
+namespace ProjectsApi.Data
+{
+    public class DataInitializer
+    {
+        public static void SeedDatabase(AppDbContext context)
+        {
+            if (!context.Projects.Any())
+            {
+                context.Projects.AddRange(
+                    new Project
+                    {
+                        Name = "BankApp",
+                        TechStack = "ASP.NET Core, Razor Pages, SQL Server",
+                        Date = new DateTime(2025, 1, 15),
+                        Description = "A simple banking system with account management and transactions.",
+                        GitHubUrl = "https://github.com/LuxmiPalma/BankAB.git",
+                        LiveDemoUrl = "https://nextgenbank-h2a2hxhqa4a2gbgx.swedencentral-01.azurewebsites.net\r\n"
+                    },
+                    new Project
+                    {
+                        Name = "Ad API",
+                        TechStack = "ASP.NET Core Web API, Entity Framework, SQL Server",
+                        Date = new DateTime(2025, 3, 1),
+                        Description = "An ad listing API similar to Blocket.se for managing ads.",
+                        GitHubUrl = "https://github.com/LuxmiPalma/BlocketAdsApi.git",
+                        //LiveDemoUrl = "https://adapi.azurewebsites.net"
+                    }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
