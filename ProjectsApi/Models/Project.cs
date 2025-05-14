1dff428b137b11476362f3d5f5fe6a10c98af706
LuxmiPalma <luxmi.palma@gmail.com>
2025-05-14 12:03:23 +0200

    Create the Project Model

---
+++ ProjectsApi/Models/Project.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectsApi.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string TechStack { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string GitHubUrl { get; set; }
+        public string LiveDemoUrl { get; set; }
+    }
+}
