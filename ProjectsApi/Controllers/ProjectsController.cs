f5e7335790c94c9a1c795d05249810a888131380
LuxmiPalma <luxmi.palma@gmail.com>
2025-05-17 20:39:30 +0200

    Update API Endpoint also use crude method

--- ProjectsApi/Controllers/ProjectsController.cs
+++ ProjectsApi/Controllers/ProjectsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProjectsApi.Data;
 using ProjectsApi.Models;
 
@@ -15,18 +16,80 @@ namespace ProjectsApi.Controllers
             _context = context;
         }
         [HttpGet]
-        public IActionResult GetProjects()
+        public async Task<ActionResult<IEnumerable<Project>>> GetProjects()
         {
-            var projects = _context.Projects.ToList();
-            return Ok(projects);
+            return await _context.Projects
+        .Include(p => p.Technologies)
+        .ThenInclude(ts => ts.TechIcon)
+        .ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Project>> GetProject(int id)
+        {
+            var project = await _context.Projects
+                .Include(p => p.Technologies)
+                .ThenInclude(t => t.TechIcon)
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (project == null)
+                return NotFound();
+
+            return project;
         }
 
         [HttpPost]
-        public IActionResult AddProject(Project project)
+        public async Task<IActionResult> AddProject(Project project)
         {
             _context.Projects.Add(project);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
+
             return CreatedAtAction(nameof(GetProjects), new { id = project.Id }, project);
         }
+
+        // PUT: api/Projects/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProject(int id, Project project)
+        {
+            if (id != project.Id)
+                return BadRequest();
+
+            _context.Entry(project).State = EntityState.Modified;
+
+            // Update TechStack manually if needed
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Projects.Any(e => e.Id == id))
+                    return NotFound();
+                else
+                    throw;
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Projects/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProject(int id)
+        {
+            var project = await _context.Projects
+                .Include(p => p.Technologies)
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (project == null)
+                return NotFound();
+
+            _context.Projects.Remove(project);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
+
+
 }
