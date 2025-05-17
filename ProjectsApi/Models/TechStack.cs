f068a1c7d00e065ab2afc3b018b56b0f7dbe6af2
LuxmiPalma <luxmi.palma@gmail.com>
2025-05-17 20:11:56 +0200

    crete model TechStack

---
+++ ProjectsApi/Models/TechStack.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjectsApi.Models
+{
+    public class TechStack
+    {
+        public int Id { get; set; }
+
+        public int ProjectId { get; set; }
+        public Project Project { get; set; } = null!;
+
+        public int TechIconId { get; set; }
+        public TechIcon TechIcon { get; set; } = null!;
+    }
+
+    
+}
