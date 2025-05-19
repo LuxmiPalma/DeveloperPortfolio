834befb8e52cc6775029d346a4f068630c2716c9
LuxmiPalma <luxmi.palma@gmail.com>
2025-05-19 13:23:24 +0200

    push

---
+++ DeveloperPortfolio/Models/WeatherApiResult.cs
@@ -0,0 +1,27 @@
+﻿namespace DeveloperPortfolio.Models
+{
+    public class WeatherApiResult
+    {
+        public Location location { get; set; }
+        public Current current { get; set; }
+    }
+    public class Location
+    {
+        public string name { get; set; }
+    }
+
+    public class Current
+    {
+        public double temp_c { get; set; }
+        public Condition condition { get; set; }
+        public double wind_kph { get; set; }
+        public double precip_mm { get; set; }
+    }
+
+    public class Condition
+    {
+        public string text { get; set; }
+        public string icon { get; set; }
+    }
+
+}
